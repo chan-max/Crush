d5a29dc10ff14c8cc06ba2b5ed7274dd0c8347d3
2651308363@qq.com <2651308363@qq.com>
2021-12-18 20:22:40 +0800

    ...

--- Crush/main.ts
+++ Crush/main.ts
@@ -2,58 +2,60 @@
 console.log("%cCrush Studio", "color: #333;  font-size: 10px; font-weight:bold;");
 
 var template: string = (document as any).querySelector('#style').innerHTML
-
-var RE_selector: RegExp = /([^{};]*)\s*{/
-
 var RE_propertyAndValue: RegExp = /^([^;{}]+)\s*:\s*([^;]+);/
-
-
+var RE_selector: RegExp = /([^{};]*)(?<!\s)\s*{/
 var $ = null
-
-var selectorStack: any = [] // 维护选择器结构
-
-var currentDeclaration:any = null
-
-var map = {}
-
+var stack: any = [] // 维护选择器结构
+var res:any = []
 /* try to get the property and value */
 while (template = template.trimLeft()) {
     if ($ = RE_propertyAndValue.exec(template)) {
+        // 处理规则属性
         var [{ length }, property, value] = $
-        currentDeclaration[property] = value
+        stack[stack.length - 1].declarations[property] = value
         template = template.substring(length)
+    } else if (template[0] === '}') {
+        // 层叠表闭合  
+        stack.pop()
+        template = template.substring(1)
+    } else if (template.startsWith('--')) {
+        // 处理指令
+    } else if (template.startsWith('@')) {
+        // 处理媒体查询，动画等 todo
     } else {
-        if (template[0] === '}') {
-            selectorStack.pop()
-            template = template.substring(1)
-        } else {
-            // 处理选择器
-            var [{ length }, selector] = RE_selector.exec(template)!
-            selectorStack.push(selector)
-            var rule = {
-                selector,
-                declarations: {}
-            }
+        // 统一当作选择器来处理
+        var [{ length }, selector] = RE_selector.exec(template)!
+        
+        var rule= {
+            type:'style',
+            selector,
+            declarations: {},
+            children:[]
+        };
 
-            var realSelector = selectorStack.join(' ')
-            currentDeclaration = map[realSelector] = {}
-            template = template.substring(length)
+        if(!stack.length){
+            res.push(rule)
+        }else{
+            stack[stack.length - 1 ].children.push(rule)
         }
+
+        stack.push(rule)
+
+        template = template.substring(length)
     }
 }
 
-console.log(map);
+console.log(res);
 
-var legal = /(?<!.)([$_a-zA-Z][$_a-zA-Z0-9]*)/g
+/*
+1 html div
+2
 
-var expression = 'num.a>0'
-
-console.log('=>',expression.replace(legal,'$'));
+*/
 
 
-function scopedExpression(expression:string,) {
-    
-}
+
+
 
 
 
--- dist/esm/main.js
+++ dist/esm/main.js
@@ -1,35 +1,48 @@
 // Crush Studio
 console.log("%cCrush Studio", "color: #333;  font-size: 10px; font-weight:bold;");
 var template = document.querySelector('#style').innerHTML;
-var RE_selector = /([^{};]*)\s*{/;
 var RE_propertyAndValue = /^([^;{}]+)\s*:\s*([^;]+);/;
+var RE_selector = /([^{};]*)(?<!\s)\s*{/;
 var $ = null;
-var selectorStack = []; // 维护选择器结构
-var currentDeclaration = null;
-var map = {};
+var stack = []; // 维护选择器结构
+var res = [];
 /* try to get the property and value */
 while (template = template.trimLeft()) {
     if ($ = RE_propertyAndValue.exec(template)) {
+        // 处理规则属性
         var [{ length }, property, value] = $;
-        currentDeclaration[property] = value;
+        stack[stack.length - 1].declarations[property] = value;
         template = template.substring(length);
     }
+    else if (template[0] === '}') {
+        // 层叠表闭合  
+        stack.pop();
+        template = template.substring(1);
+    }
+    else if (template.startsWith('--')) ;
+    else if (template.startsWith('@')) ;
     else {
-        if (template[0] === '}') {
-            selectorStack.pop();
-            template = template.substring(1);
+        // 统一当作选择器来处理
+        var [{ length }, selector] = RE_selector.exec(template);
+        var rule = {
+            type: 'style',
+            selector,
+            declarations: {},
+            children: []
+        };
+        if (!stack.length) {
+            res.push(rule);
         }
         else {
-            // 处理选择器
-            var [{ length }, selector] = RE_selector.exec(template);
-            selectorStack.push(selector);
-            var realSelector = selectorStack.join(' ');
-            currentDeclaration = map[realSelector] = {};
-            template = template.substring(length);
+            stack[stack.length - 1].children.push(rule);
         }
+        stack.push(rule);
+        template = template.substring(length);
     }
 }
-console.log(map);
-var legal = /(?<!.)([$_a-zA-Z][$_a-zA-Z0-9]*)/g;
-var expression = 'num.a>0';
-console.log('=>', expression.replace(legal, '$'));
+console.log(res);
+/*
+1 html div
+2
+
+*/
