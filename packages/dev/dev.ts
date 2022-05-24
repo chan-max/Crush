04ec0627606bb942a1f75a45e21de8f99ce3d89a
2651308363@qq.com <2651308363@qq.com>
2022-05-24 14:45:08 +0800

    scheduler for hooks

--- packages/dev/dev.ts
+++ packages/dev/dev.ts
@@ -9,29 +9,18 @@ var root = {
             mounted() {
                 console.log('d1 mounted');
             },
-            updated() {
+            updated(el, dirInfo) {
                 console.log('d1 updated');
+                console.log(el, dirInfo);
             },
             unmounted() {
                 console.log('d1 unmounted');
             }
-        },
-        d2: {
-            mounted() {
-                console.log('d2 mounted');
-            },
-            updated() {
-                console.log('d2 updated');
-            },
-            unmounted() {
-                console.log('d2 unmounted');
-            }
-        }
+        }    
     },
     template: `
             <button @click="count++"> {{ count }} </button>
-            <h1 --d1:a:b:c.x.y.z="count" --if="count%2===0"> 111 </h1>
-            <h1 --d2:a:b:c.x.y.z="count" --if="count%2===1"> 222 </h1>
+            <h1 --d1:a:b:c.x.y.z="count"   > 111 </h1>
         `,
     create($: any) {
         $.count = 0
